35bf4570c1bc795c024db939aa061b69af95f0af
Danil Komyshev <40982684+danil99152@users.noreply.github.com>
2019-07-01 10:54:50 +0400

    start

---
+++ Resume/Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿using Resume.Models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Resume.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult MyMethod()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult MyMethod(Person person)
+        {
+            StreamWriter sw = new StreamWriter("C:\\Resume.txt", true);
+
+            sw.WriteLine($"ФИО: {person.FIO}");
+
+            sw.WriteLine($"Дата рождения: {person.Birthday}");
+
+            sw.WriteLine($"Прошлые места работы: {person.PastPlaces}");
+
+            sw.WriteLine($"О себе: {person.About}");
+
+            sw.Close();
+
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ Resume/Models/Person.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Web;
+
+namespace Resume.Models
+{
+    public class Person
+    {
+        public int Id { get; set; }
+        [DisplayName("Ваше Фамилия, Имя и Отчество(если имеется)")]
+        public string FIO { get; set; }
+        [DisplayName("Ваша дата рождения")]
+        public DateTime Birthday { get; set; }
+        [DisplayName("Напишите прошлые места работы")]
+        public string PastPlaces { get; set; }
+        [DisplayName("Расскажите о себе")]
+        public string About { get; set; }
+    }
+}
